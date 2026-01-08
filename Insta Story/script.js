7795c5efd820ffe7cc42a02456540933b1603805
Shivansh <abhinavnerd@gmail.com>
2026-01-09 00:42:12 +0530

    Add Insta Story project

---
+++ Insta Story/script.js
@@ -0,0 +1,42 @@
+let arr = [
+  {
+    dp: "https://images.unsplash.com/photo-1510706019500-d23a509eecd4?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    story:
+      "https://plus.unsplash.com/premium_photo-1669704098750-7cd22c35422b?q=80&w=688&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    dp: "https://plus.unsplash.com/premium_photo-1682095757120-c9abb908ed60?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    story:
+      "https://plus.unsplash.com/premium_photo-1669704099171-b47c80016430?q=80&w=688&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    dp: "https://plus.unsplash.com/premium_photo-1690407617542-2f210cf20d7e?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    story:
+      "https://images.unsplash.com/photo-1516726817505-f5ed825624d8?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    dp: "https://images.unsplash.com/photo-1516726817505-f5ed825624d8?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    story:
+      "https://images.unsplash.com/photo-1524502397800-2eeaad7c3fe5?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+];
+
+let slug = "";
+
+arr.forEach(function (elem, idx) {
+  slug += `<div id="story"> <img id="${idx}" src="${elem.dp}" /> </div>`;
+});
+
+let strs = document.querySelector("#stories");
+let fs = document.querySelector("#full-screen");
+
+strs.innerHTML = slug;
+
+strs.addEventListener("click", (dets) => {
+  fs.style.display = "block";
+  fs.style.backgroundImage = `url(${arr[dets.target.id].story})`;
+
+  setTimeout(function () {
+    fs.style.display = "none";
+  }, 3000);
+});
